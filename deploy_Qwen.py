aaa8f152335b6f259f4241a11f1e76e897597965
YaoG722 <2297105681@qq.com>
2025-04-19 15:16:18 +0800

    调整模型最大上下文长度至25600，并将最大序列数设置为64

--- deploy_Qwen.py
+++ deploy_Qwen.py
@@ -80,10 +80,10 @@ if __name__ == "__main__":
             # f"--tensor-parallel-size 8 "  # GPU 并行数，单卡部署需关闭
             f"--host {host} --port {ports[i]} "  # 绑定IP和端口
             f"--gpu-memory-utilization 0.8 "  # 设置GPU显存利用率阈值
-            f"--max-model-len 32768 "  # 设置模型最大上下文长度
+            f"--max-model-len 25600 "  # 设置模型最大上下文长度
             # f"--enable-chunked-prefill "  # 启用分块预填充
             # f"--max-num-batched-tokens 2048 "  # 最大批处理令牌数
-            f"--max-num-seqs 128 "  # 最大序列数
+            f"--max-num-seqs 64 "  # 最大序列数
             f"--disable-log-stats")  # 关闭性能统计日志
         t = threading.Thread(target=subprocess.run,
                                 args=(cmd, ),
